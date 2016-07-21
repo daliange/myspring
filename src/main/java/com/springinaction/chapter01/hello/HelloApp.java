d071ce1078896e3c192ed28636208653800a3b1d
li.jie <li.jie@sand.com.cn>
2016-07-21 19:44:02 +0800

    myspring

---
+++ src/main/java/com/springinaction/chapter01/hello/HelloApp.java
@@ -0,0 +1,29 @@
+package com.springinaction.chapter01.hello;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class HelloApp {
+	
+	private static Logger logger = Logger.getLogger(HelloApp.class);  
+	
+  public static void main(String[] args) throws Exception {
+    BeanFactory factory = 
+        new XmlBeanFactory(new ClassPathResource("hello.xml"));
+    
+    // 记录debug级别的信息  
+    logger.debug("This is debug message.");  
+    // 记录info级别的信息  
+    logger.info("This is info message.");  
+    // 记录error级别的信息  
+    logger.error("This is error message.");  
+    
+    GreetingService greetingService = 
+        (GreetingService) factory.getBean("greetingService");
+
+    greetingService.sayGreeting();
+  }
+}
