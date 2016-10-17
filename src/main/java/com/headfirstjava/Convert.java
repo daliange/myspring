6250cf4776abfc5a6f78fbfc88fd346c00d97e48
li.jie <li.jie@sand.com.cn>
2016-10-17 23:00:38 +0800

    java类型转换

---
+++ src/main/java/com/headfirstjava/Convert.java
@@ -0,0 +1,14 @@
+package com.headfirstjava;
+
+public class Convert {
+	
+	public static void main(String[] args) {
+		int x = 24 ;
+//		byte y = x ;
+		
+		byte a = 25 ; 
+		int b = a ;
+		System.out.println(b);
+	}
+
+}
